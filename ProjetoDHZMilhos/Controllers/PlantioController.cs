534883a8a354ef5efaf7fd3586b077cf0d3164af
Guilherme Bueno Zago <guilherme.zago@alunos.sc.senac.br>
2025-01-27 20:42:33 -0300

    Implementação do Plantio.

---
+++ ProjetoDHZMilhos/Controllers/PlantioController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoDHZMilhos.Models.Plantio;
+
+namespace ProjetoDHZMilhos.Controllers
+{
+    public class PlantioController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            var plantios = new List<PlantioModel>()
+            {
+                new PlantioModel()
+                {
+                    Id = 1,
+                    Plantacao_Id = 1,
+                    Data_Plantio = new DateTime(2025, 1, 27), 
+                    Tipo_Milho = "feio",
+                    Quantidade_Sementes = 5000,
+                    Hectares_Plantados = 100,
+                }
+            };
+
+            var model = new PlantiosModel()
+            {
+                Plantios = plantios
+            };
+
+            return View(model);
+        }
+    }
+}
--- ProjetoDHZMilhos/Models/Plantio/PlantioModel.cs
+++ ProjetoDHZMilhos/Models/Plantio/PlantioModel.cs
@@ -6,7 +6,7 @@
         public int Plantacao_Id { get; set; }
         public DateTime Data_Plantio { get; set; }
         public string Tipo_Milho { get; set; }
-        public string Quantidade_Sementes { get; set; }
-
+        public int Quantidade_Sementes { get; set; }
+        public int Hectares_Plantados { get; set; }
     }
 }

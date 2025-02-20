8689191061b11f0a4b7f482e469571ab589a2643
JessAgnolo <jess.agnolo@gmail.com>
2025-02-19 21:44:56 -0300

    Correção entidade base

--- MinhaPrimeiraAplicacao.Utils/Entidades/EntidadeBase.cs
+++ MinhaPrimeiraAplicacao.Utils/Entidades/EntidadeBase.cs
@@ -9,7 +9,7 @@ using MySql.Data.MySqlClient;
 
 namespace MinhaPrimeiraAplicacao.Utils.Entidades
 {
-    public abstract class EntidadeBase1<T>
+    public abstract class EntidadeBase<T>
     {
         public long ID { get; set; }
         protected abstract string TableName { get; }
@@ -87,68 +87,6 @@ namespace MinhaPrimeiraAplicacao.Utils.Entidades
 
                 cmd.ExecuteNonQuery();
             }
-        }
-
-        public void Delete()
-        {
-            using (MySqlConnection conn = new MySqlConnection(DBConnection.CONNECTION_STRING))
-            {
-                conn.Open();
-
-                var cmd = conn.CreateCommand();
-                cmd.Parameters.Add(new MySqlParameter("pID", ID));
-                cmd.CommandText = @$"DELETE FROM {TableName} WHERE ID = @pID";
-
-                cmd.ExecuteNonQuery();
-            }
-        }
-    }
-
-    public abstract class EntidadeBase
-    {
-        public long ID { get; set; }
-        protected abstract string TableName { get; }
-        protected abstract List<string> Fields { get; }
-
-        protected abstract EntidadeBase Fill(MySqlDataReader reader);
-
-        protected EntidadeBase BaseGet(long id)
-        {
-            using (MySqlConnection conn = new MySqlConnection(DBConnection.CONNECTION_STRING))
-            {
-                conn.Open();
-                var query = $"SELECT {string.Join(", ", Fields)} FROM {TableName} WHERE ID = @ID";
-                var cmd = new MySqlCommand(query, conn);
-                cmd.Parameters.Add(new MySqlParameter("ID", id));
-
-                var reader = cmd.ExecuteReader();
-                if (reader.Read())
-                {
-                    return Fill(reader);
-                }
-            }
-
-            return null;
-        }
-
-        protected List<EntidadeBase> BaseGetAll()
-        {
-            var result = new List<EntidadeBase>();
-
-            using (MySqlConnection conn = new MySqlConnection(DBConnection.CONNECTION_STRING))
-            {
-                conn.Open();
-                var query = @$"SELECT {string.Join(", ", Fields)} FROM {TableName}";
-                var cmd = new MySqlCommand(query, conn);
-
-                var reader = cmd.ExecuteReader();
-                while (reader.Read())
-                {
-                    result.Add(Fill(reader));
-                }
-            }
-
-            return result;
         }
 
         public void Delete()
